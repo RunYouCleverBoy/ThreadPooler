dec053481cdf95b84c5c2b0364b6dd61380f37b2
Shmuel Meymann <shmuel@nowforce.com>
2016-03-05 21:07:28 +0200

    Initial draft

---
+++ threadinger/src/main/java/threadinger/runyoucleverboy/git/thredinger/LooperThread.java
@@ -0,0 +1,101 @@
+package threadinger.runyoucleverboy.git.thredinger;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Description:
+ * Created by RunYouCleverBoy on 5.3.16.
+ */
+@SuppressWarnings("unused")
+public class LooperThread {
+    public static final int INVALID_DELAY = -1;
+    private Thread thread;
+    private Handler handler;
+    private LinkedList<EarlyRunnable> earlyRunnables = new LinkedList<>();
+    private AtomicBoolean available = new AtomicBoolean(true);
+
+    public LooperThread() {
+        thread = new Thread(new LooperRunnable());
+    }
+
+    public synchronized LooperThread shutdown() {
+        available.set(false);
+        try {
+            post(new PoisonPill());
+            thread.join();
+            thread = null;
+        } catch (InterruptedException ignored) {}
+        return this;
+    }
+
+    public synchronized LooperThread post(Runnable runnable) {
+        return post(runnable, INVALID_DELAY);
+    }
+
+    public synchronized LooperThread post(Runnable runnable, int delay) {
+        validateRuntime();
+        if (handler != null) {
+            if (delay > 0) {
+                handler.postDelayed(runnable, delay);
+            } else {
+                handler.post(runnable);
+            }
+        } else {
+            earlyRunnables.add(new EarlyRunnable(runnable, delay));
+        }
+
+        return this;
+    }
+
+    private synchronized void handleEarlyRunnables() {
+        handler = new Handler();
+        for (EarlyRunnable runnable: earlyRunnables) {
+            post(runnable.runnable, runnable.delay);
+        }
+    }
+
+    private void validateRuntime() {
+        if (!available.get()) {
+            throw new NotReadyException();
+        }
+    }
+
+    private class LooperRunnable implements Runnable {
+        @Override
+        public void run() {
+            Looper.prepare();
+            handleEarlyRunnables();
+            Looper.loop();
+        }
+    }
+
+    private class PoisonPill implements Runnable {
+        @Override
+        public void run() {
+            Looper looper = Looper.myLooper();
+            if (looper != null) {
+                looper.quit();
+            }
+        }
+    }
+
+    private class EarlyRunnable {
+        private Runnable runnable;
+        private int delay;
+
+        public EarlyRunnable(Runnable runnable, int delay) {
+            this.runnable = runnable;
+            this.delay = delay;
+        }
+    }
+
+    private class NotReadyException extends RuntimeException {
+        public NotReadyException() {
+            super("Looper thread is not active anymore");
+        }
+    }
+}
---
+++ threadinger/src/main/java/threadinger/runyoucleverboy/git/thredinger/ThreadPool.java
@@ -0,0 +1,79 @@
+package threadinger.runyoucleverboy.git.thredinger;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@SuppressWarnings("unused")
+public class ThreadPool {
+    public final int POOL_SIZE = 10;
+    private final ExecutorService threadPool;
+    private final AtomicInteger inventorySize;
+
+    public ThreadPool() {
+        threadPool = Executors.newFixedThreadPool(POOL_SIZE);
+        inventorySize = new AtomicInteger(POOL_SIZE);
+    }
+
+    public void shutdown() {
+        threadPool.shutdown();
+    }
+
+    public <T> Future<T> submitCallable(final Callable<T> command, final String name) {
+        return threadPool.submit(new CommandWrapper<>(command, name));
+    }
+
+    public void submit(final Runnable command, final String name, final Runnable whenDone) {
+        threadPool.submit(new Runnable() {
+            @Override
+            public void run() {
+                setCurrentThreadName(name);
+                inventorySize.decrementAndGet();
+                command.run();
+                if (whenDone != null) {
+                    whenDone.run();
+                }
+                inventorySize.incrementAndGet();
+            }
+        });
+    }
+
+    public void submit(final Runnable command) {
+        threadPool.submit(new Runnable() {
+            @Override
+            public void run() {
+                setCurrentThreadName("ThreadPoolRunnable" + System.nanoTime());
+                inventorySize.decrementAndGet();
+                command.run();
+                inventorySize.incrementAndGet();
+            }
+        });
+    }
+
+    private void setCurrentThreadName(String name) {
+        if (name != null && !name.isEmpty()) {
+            Thread.currentThread().setName(name);
+        }
+    }
+
+    private class CommandWrapper<T> implements Callable<T>{
+        private final Callable<T> command;
+        private final String name;
+
+        public CommandWrapper(Callable<T> command, String name) {
+            this.command = command;
+            this.name = name;
+        }
+
+        @Override
+        public T call() throws Exception {
+            setCurrentThreadName(name);
+            inventorySize.decrementAndGet();
+            T result = command.call();
+            inventorySize.incrementAndGet();
+            return result;
+        }
+    }
+}
